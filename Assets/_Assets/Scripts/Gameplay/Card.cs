9d85fc12c1ae5f400e3bea9ed427b78012206397
R1nge <r1ngegamedev@gmail.com>
2024-04-01 12:09:03 +0300

    Bounce when clicking an incorrect card

--- Assets/_Assets/Scripts/Gameplay/Card.cs
+++ Assets/_Assets/Scripts/Gameplay/Card.cs
@@ -1,27 +1,53 @@
+using System;
 using _Assets.Scripts.Configs;
+using DG.Tweening;
 using UnityEngine;
 
 namespace _Assets.Scripts.Gameplay
 {
     public class Card : MonoBehaviour
     {
+        [SerializeField] private float tweenOffset = 0.5f;
+        [SerializeField] private float tweenDuration = 0.25f;
         [SerializeField] private SpriteRenderer spriteRenderer;
         private bool _isTarget;
         private CardData _cardData;
+        private Vector3 _startPosition;
         public bool IsTarget => _isTarget;
         public CardData CardData => _cardData;
+        private Sequence _sequence;
 
         public void Init(CardData cardData)
         {
             _cardData = cardData;
             spriteRenderer.sprite = cardData.Sprite;
+            _startPosition = transform.localPosition;
+
+            _sequence = DOTween.Sequence();
+            _sequence.Append(transform.DOLocalMoveX(_startPosition.x + tweenOffset, tweenDuration));
+            _sequence.Append(transform.DOLocalMoveX(_startPosition.x - tweenOffset, tweenDuration));
+            _sequence.Append(transform.DOLocalMoveX(_startPosition.x, tweenDuration));
+            _sequence.Pause();
+            _sequence.SetAutoKill(false);
         }
 
         public void SetTarget(bool isTarget) => _isTarget = isTarget;
 
         public void Interact()
         {
-            //TODO: play tween
+            if (!_isTarget)
+            {
+                BounceTween();
+            }
+            else
+            {
+                BounceTween();
+                //TODO: add stars patricles
+            }
         }
+
+        private void BounceTween() => _sequence.Restart();
+
+        private void OnDestroy() => _sequence.Kill();
     }
 }
